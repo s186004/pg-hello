7fe0c76e3ff1e956ec27542531f1f50b81a803d5
monika bujas <monikabujas1@gmail.com>
2018-03-09 13:20:42 +0100

    'motion'

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -40,3 +40,13 @@ function checkConnection() {
 
     navigator.notification.alert('Typ połączenia: ' + states[networkState]);
 }
+function onSuccess(acceleration) {
+    alert('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
+}
+
+function onError() {
+    alert('onError!');
+}
